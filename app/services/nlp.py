97c4748f9ef5ac4a7b6c0782419c9afefcb3b4a5
Luis5Felipe <luisfelipedos29@gmail.com>
2026-01-18 19:08:02 -0300

    fix: resolvendo bug do nlp.py
    # Conflicts:
    #	app/services/nlp.py
    #	requirements.txt

--- app/services/nlp.py
+++ app/services/nlp.py
@@ -1,12 +1,14 @@
 import spacy
+import subprocess
 
 _nlp = None
 
 def get_nlp():
-    global _nlp
-    if _nlp is None:
-        _nlp = spacy.load("pt_core_news_sm")
-    return _nlp
+    try:
+        return spacy.load("pt_core_news_sm")
+    except OSError:
+        subprocess.run(["python", "-m", "spacy", "download", "pt_core_news_sm"])
+        return spacy.load("pt_core_news_sm")
 
 def preprocess(text: str) -> str:
     nlp = get_nlp()
