509330c110e92a4a0592cece6ae2470a9f65725a
Luis5Felipe <luisfelipedos29@gmail.com>
2026-01-18 01:43:01 -0300

    feat: classificador adicionado

--- app/main.py
+++ app/main.py
@@ -2,24 +2,21 @@ from fastapi import FastAPI, UploadFile, File
 from app.services.file_reader import read_txt, read_pdf
 from app.services.nlp import preprocess
 from app.services.classifier import classify_email
-from app.services.responder import generate_response
-
 app = FastAPI(title="Classificador de Emails")
 
 @app.post("/classificar")
 async def classificar_email(file: UploadFile = File(...)):
     if file.filename.endswith(".txt"):
-        text = read_txt(await file.read())
+        content = await file.read()
+        text = read_txt(content)
     elif file.filename.endswith(".pdf"):
         text = read_pdf(file.file)
     else:
         return {"erro": "Formato não suportado"}
-
     clean_text = preprocess(text)
-    category = classify_email(clean_text)
-    response = generate_response(category)
+    categoria, resposta_sugerida = classify_email(clean_text)
 
     return {
-        "categoria": category,
-        "resposta_sugerida": response
+        "categoria": categoria,
+        "resposta_sugerida": resposta_sugerida
     }
